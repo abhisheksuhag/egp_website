56e7d2c1e6b6cff42a5a9cb325beb40cbf33dfba
Abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-06-27 15:32:19 +0530

    fgh

--- Pages/src/Components/AboutUs2.jsx
+++ Pages/src/Components/AboutUs2.jsx
@@ -6,21 +6,18 @@ const AboutUs2 = () => {
     {
       id: 1,
       heading: "WHO WE ARE",
-      subheading: "Resource management and circular economy",
       paragraph:
         "Founded in early 2021, and headquartered in National Capital Region of India, ESG Global Partners (EGP) has been set up as a global provider of social and environmental consulting services. EGP is an association of world class professionals – Business Leaders, Project Managers, Thematic Area Experts, Scientists, Engineers & Analysts, who have come together from different geographies, to deliver best in class ESG services.",
     },
     {
       id: 2,
       heading: "SUSTAINABILITY",
-      subheading: "Sustainability and the future",
       paragraph:
         "At EGP, we are committed to the ethos of sustainability and attempt to embed the same in all our practices & solutions we offer. We intend to address challenges by providing sustainable solutions which would have a positive impact on the environment and communities.",
     },
     {
       id: 3,
       heading: "OUR PEOPLES",
-      subheading: "Modern solutions",
       paragraph:
         "Our team comprises of in-house Senior ESG Professionals, Environmental Scientists, Engineers, Social Scientists, Ecologists, Project Managers, etc. who have 10+ years of experience in various thematic areas. Our people are at the heart of our business, and we are committed to developing an organisation where people are their best, feel valued, and are safe. Our experts bring 30+ years of collective industry, leadership and technical experience in 41+ countries!",
     },
@@ -65,9 +62,7 @@ const AboutUs2 = () => {
   return (
     <div className="h-screen relative flex flex-col lg:flex-row">
       {/* Main Heading shifted to the left */}
-      <h1 className="text-4xl font-bold text-gray-800 p-4 lg:absolute lg:top-10 lg:left-3 lg:ml-10 z-20">
-        Market
-      </h1>
+      
 
       {/* Text Carousel */}
       <div className="text-background-container w-full lg:w-[42%] lg:ml-10 h-screen bg-gray-300 z-10 overflow-hidden" ref={textScrollRef}>
@@ -76,7 +71,7 @@ const AboutUs2 = () => {
           {textSections.map((section) => (
             <div key={section.id} className="w-full h-full flex-shrink-0">
               <div className="p-8 bg-transparent z-10 flex-shrink-0">
-                <h2 className="subheading text-lg mt-20 mb-10 mr-10">{section.subheading}</h2>
+                <h2 className="subheading text-lg mt-20 mb-10 mr-10"> </h2>
                 <h1 className="heading text-3xl mb-16 mr-20">{section.heading}</h1>
                 <p className="paragraph text-lg mr-20 font-normal">
                   {section.paragraph}
--- Pages/src/Components/ContactPage.jsx
+++ Pages/src/Components/ContactPage.jsx
@@ -1,4 +1,4 @@
-import  { useRef } from 'react';
+import { useRef } from 'react';
 import emailjs from 'emailjs-com';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -66,17 +66,15 @@ const ContactPage = () => {
                     <div className="text-white space-y-6">
                         <div>
                             <div className="flex items-center mb-2">
-                                <svg className="w-6 h-6 sm:w-8 sm:h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 2l4 4m0 0l-6.586 6.586a2 2 0 01-.707.293l-3.757.752a1 1 0 01-1.179-1.179l.752-3.757a2 2 0 01.293-.707L18 6m4-4L10 10m-7 5h3m4 0h6m-4-4h4m-4 4a9 9 0 110-18 9 9 0 0118 0z"></path>
-                                </svg>
+                            <svg src="phone.svg"></svg>
                                 <span className="ml-2 text-sm sm:text-lg">Email</span>
                             </div>
                             <p className="text-sm sm:text-lg">support@esggp.org</p>
                         </div>
                         <div>
                             <div className="flex items-center mb-2">
-                                <svg className="w-6 h-6 sm:w-8 sm:h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 10l1-1m10-6H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2v-5M16 2h6m-3 3v6m0 0h6m-6 0H7a4 4 0 00-4 4v4a2 2 0 002 2h12a2 2 0 002-2v-5a9 9 0 00-9-9H4z"></path>
+                                <svg className="w-6 h-6 sm:w-8 sm:h-8 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="email.svg" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                 </svg>
                                 <span className="ml-2 text-sm sm:text-lg">Phone</span>
                             </div>
@@ -84,8 +82,8 @@ const ContactPage = () => {
                         </div>
                         <div>
                             <div className="flex items-center mb-2">
-                                <svg className="w-6 h-6 sm:w-8 sm:h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c1.38 0 2.5 1.12 2.5 2.5S13.38 13 12 13s-2.5-1.12-2.5-2.5S10.62 8 12 8zM5.5 21a2.5 2.5 0 01-2.5-2.5A2.5 2.5 0 015.5 16h13a2.5 2.5 0 012.5 2.5A2.5 2.5 0 0118.5 21h-13zM12 3c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4z"></path>
+                                <svg className="w-6 h-6 sm:w-8 sm:h-8 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 8c1.38 0 2.5 1.12 2.5 2.5S13.38 13 12 13s-2.5-1.12-2.5-2.5S10.62 8 12 8zM5.5 21a2.5 2.5 0 01-2.5-2.5A2.5 2.5 0 015.5 16h13a2.5 2.5 0 012.5 2.5A2.5 2.5 0 0118.5 21h-13zM12 3c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                 </svg>
                                 <span className="ml-2 text-sm sm:text-lg">Working hours</span>
                             </div>
